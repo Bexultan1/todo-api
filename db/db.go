571b0e03bf4b0ce3d17a97583ac879e239950b19
Bexultan <loliklolik449@gmail.com>
2026-06-15 11:24:14 +0500

    add env variebles

--- db/db.go
+++ db/db.go
@@ -1,17 +1,29 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sqlx.DB
 
 func Connect() {
+	godotenv.Load(".env")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
 	var err error
-	DB, err = sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=Bexmeen1111 dbname=todo sslmode=disable")
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	DB, err = sqlx.Connect("postgres", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
